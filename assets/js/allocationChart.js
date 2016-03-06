c638a45194ab76af00aa43e91aa3a9ffc8290e70
Sean Helvey <seanhelvey@Seans-MBP-2.PK5001Z>
2016-03-06 14:08:48 -0700

    lato vane, no good bad on thermo, evenly sized tools

---
+++ assets/js/allocationChart.js
@@ -0,0 +1,48 @@
+
+var chart = c3.generate({
+	size: {
+		height: 320,
+		width: 200
+	},
+    data: {
+        columns: [
+            ['Cash', 0],
+            ['Stock', 0]
+        ],
+        type: 'bar'
+    },
+	axis: {
+	  y: {
+	  	max: 100,
+	    tick: {
+	      values: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+	    }
+	  }
+	},
+    bar: {
+        width: {
+            ratio: 0.5 // this makes bar width 50% of length between ticks
+        }
+        // or
+        //width: 100 // this makes bar width 100px
+    },
+	legend: {
+	  position: 'bottom'
+	},    
+	tooltip: {
+	  format: {
+	    title: function () { return 'Allocation' }
+	  }
+	}  
+});
+
+setTimeout(function () {
+    chart.load({
+        columns: [
+            ['Cash', 25],
+            ['Stock', 75]
+        ]
+    });
+}, 500);
+
+$("#allocationChart").append(chart.element);

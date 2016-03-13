4b7fae6f681403699d076173033d6a206c54c72a
Sean Helvey <seanhelvey@Seans-MacBook-Pro-2.local>
2016-03-13 08:43:16 -0600

    big changes price gauge, assets, file strucutre

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -4,6 +4,20 @@ $('#exampleModal').on('show.bs.modal', function (event) {
   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
   var modal = $(this)
-  modal.find('.modal-title').text(dada)
-  modal.find('.modal-body p').text(dada + " right here.")
+  var body = "";
+  if (dada === "Meter") {
+  	title = "Value Meter";
+  	body = "A higher level on the meter indicates good value, while a lower level implies relatively poor value. The meter is based on risk adjusted changes in credit spreads." 
+  } else if (dada === "Vane") {
+  	title = "Price Forecast";
+  	body = "More info coming soon!";  	
+  } else if (dada === "Allocation") {
+  	title = "Asset Allocation";
+  	body = "More info coming soon!";  	
+  } else if (dada === "Ranking") {
+  	title = "Selection Process";
+  	body = "More info coming soon!";  	
+  }
+  modal.find('.modal-title').text(title);
+  modal.find('.modal-body p').text(body);
 })    

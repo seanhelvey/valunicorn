a0164125e5e17c67f4bd9c21d241a0fbd23987b8
Sean Helvey <seanhelvey@Seans-MacBook-Pro-2.local>
2017-02-10 21:52:57 -0700

    feb update

--- assets/js/taps.js
+++ assets/js/taps.js
@@ -52,19 +52,19 @@ $('#current').on('tap', function(e){
 });
 
 function loadCurrent() {
-    thermometer("thermo1", 13, 9, true);
+    thermometer("thermo1", 13, 8, true);
 
     chart.load({
         columns: [
-            ['Cash', 33],
-            ['Stock', 67]
+            ['Cash', 42],
+            ['Stock', 58]
         ]
     });
 
     $('#weather-vane').css({
-        '-ms-transform': 'rotate(15deg)',
-        '-webkit-transform': 'rotate(15deg)',
-        'transform': 'rotate(15deg)',
+        '-ms-transform': 'rotate(7.5deg)',
+        '-webkit-transform': 'rotate(7.5deg)',
+        'transform': 'rotate(7.5deg)',
     });
 }
 
